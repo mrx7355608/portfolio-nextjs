8e3fc68e6b2bf2177cd642a86c2e698b34bc8ca4
mrx7355608 <mrx7355608@gmail.com>
2024-05-26 10:39:02 +0500

    created about section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,14 @@
+import About from "@/components/About";
 import Hero from "@/components/Hero";
+import { Box } from "@chakra-ui/react";
 
 export default function Home() {
   return (
     <main>
       <Hero />
+      <Box w={"80%"} mx={"auto"}>
+        <About />
+      </Box>
     </main>
   );
 }
---
+++ src/components/About.tsx
@@ -0,0 +1,36 @@
+import { Text, Box, Heading, HStack } from "@chakra-ui/react";
+import Image from "next/image";
+import { Jockey_One } from "next/font/google";
+
+const jockeyOne = Jockey_One({ weight: ["400"], subsets: ["latin"] });
+
+export default function About() {
+  return (
+    <Box
+      w={"65%"}
+      textAlign={"center"}
+      color={"white"}
+      gap={8}
+      my={36}
+      mx={"auto"}
+    >
+      <Heading
+        fontFamily={jockeyOne.style.fontFamily}
+        as="h1"
+        mb={4}
+        fontSize={"5xl"}
+      >
+        About me
+      </Heading>
+      <Text fontWeight={400} color={"#a1a1a1"} lineHeight={7}>
+        I am a fullstack developer with strong foundations in MERN stack
+        development. I am constantly expanding my skillset by learning Nextjs to
+        get familiar with this new framework and to build performant websites.
+        My academic journey is ongoing at Iqra Univeristy, where I am pursuing
+        my degree in Computer Science. Beyond MERN stack, I also have worked
+        with other languages like Java, C++, and Python. I am also a big fan of
+        linux operating systems because of the productivity boost they provide.
+      </Text>
+    </Box>
+  );
+}
